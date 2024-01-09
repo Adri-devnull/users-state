29dfeb60b51595c4ed25ea4392eae6472ac55755
Adri-devnull <adri.devnull@gmail.com>
2024-01-09 21:42:57 +0100

    exercise finished

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -4,31 +4,31 @@ import Users from '../users/Users';
 import { StyledContainerList, StyledList, StyledUserList } from './styles';
 
 const List = () => {
-	const [filteredUsers, setFilteredUsers] = useState(USERS);
+	const [active, setActive] = useState(false);
+	const [inputValue, setInputValue] = useState('');
+	const [forName, setForName] = useState(0);
 
-	const filteredByName = event => {
-		const letters = event.target.value;
-		const names = USERS.filter(user => user.name.startsWith(letters));
-		console.log(letters);
-		console.log(names);
-	};
+	let filteredUsers = filterActiveUsers(USERS, active);
+	filteredUsers = filterForName(filteredUsers, inputValue);
+	filteredUsers = filterAlphabetically(filteredUsers, forName);
 
 	return (
 		<StyledList>
 			<h1>Listado de usuarios</h1>
 			<StyledContainerList>
-				<input type='text' onChange={event => filteredByName(event)} />
+				<input
+					type='text'
+					onInput={event => handleinputValue(event, setInputValue)}
+				/>
 				<div>
 					<label htmlFor='active'>Solo activos</label>
 					<input
 						type='checkbox'
 						id='active'
-						onChange={event =>
-							showActiveorInactive(event, filteredUsers, setFilteredUsers)
-						}
+						onChange={() => handleChange(active, setActive)}
 					/>
 				</div>
-				<select>
+				<select onChange={event => handleForNameSearch(event, setForName)}>
 					<option value='0'>Por Defecto</option>
 					<option value='1'>Por Nombre</option>
 				</select>
@@ -42,12 +42,43 @@ const List = () => {
 	);
 };
 
-const showActiveorInactive = (event, filteredUsers, setFilteredUsers) => {
-	if (event.target.checked) {
-		const onlyActiveUsers = filteredUsers.filter(user => user.active);
-		setFilteredUsers(onlyActiveUsers);
-	} else {
-		setFilteredUsers(USERS);
-	}
+const handleChange = (active, setActive) => {
+	setActive(!active);
 };
+
+const filterActiveUsers = (users, active) => {
+	if (active) {
+		return users.filter(user => user.active);
+	}
+	return [...users];
+};
+
+const handleinputValue = (event, setInputValue) => {
+	const search = event.target.value;
+	setInputValue(search);
+};
+
+const filterForName = (users, inputValue) => {
+	if (!inputValue) return [...users];
+	return users.filter(user =>
+		user.name.toLowerCase().includes(inputValue.toLowerCase())
+	);
+};
+
+const handleForNameSearch = (event, setForName) => {
+	const value = event.target.value;
+	setForName(value);
+};
+
+const filterAlphabetically = (users, forName) => {
+	const sortedUsers = [...users];
+	const formatedValue = Number(forName);
+	if (formatedValue === 0) return sortedUsers;
+	return sortedUsers.sort((a, b) => {
+		if (a.name > b.name) return 1;
+		if (a.name < b.name) return -1;
+		return 0;
+	});
+};
+
 export default List;
